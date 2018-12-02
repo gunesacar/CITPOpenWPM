7feb58399472b011d506f831d3b2f95caf69d09b
Gunes Acar <gunes.acar@esat.kuleuven.be>
2018-12-01 23:27:36 -0500

    Don't store screenshots if the screen doesn't change.

--- automation/Commands/utils/screen_capture.py
+++ automation/Commands/utils/screen_capture.py
@@ -1,9 +1,23 @@
+from os.path import join
 from ...MPLogger import loggingclient
 from ...Commands import browser_commands
 
 from time import sleep, time
 from datetime import datetime
 from urlparse import urlparse
+import binascii
+import base64
+
+
+def save_screenshot_b64(out_png_path, image_b64, logger):
+    try:
+        with open(out_png_path, 'wb') as f:
+            f.write(base64.b64decode(image_b64.encode('ascii')))
+    except Exception as exc:
+        logger.exception("Error while saving screenshot on %s"
+                         % (out_png_path))
+        return False
+    return True
 
 
 def capture_screenshots(visit_duration, **kwargs):
@@ -12,16 +26,22 @@ def capture_screenshots(visit_duration, **kwargs):
     visit_id = kwargs['visit_id']
     manager_params = kwargs['manager_params']
     logger = loggingclient(*manager_params['logger_address'])
+    screenshot_dir = manager_params['screenshot_path']
+    screenshot_base_path = join(screenshot_dir, "%d_%s" % (
+        visit_id, urlparse(driver.current_url).hostname))
+    last_image_crc = 0
     for idx in xrange(0, visit_duration):
-        timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
-        suffix = "%s_%s_%d" % (urlparse(driver.current_url).hostname,
-                               timestamp,
-                               idx)
         capture_duration = 0
         try:
             t0 = time()
-            browser_commands.save_screenshot(visit_id, None, driver,
-                                             manager_params, suffix)
+            img_b64 = driver.get_screenshot_as_base64()
+            new_image_crc = binascii.crc32(img_b64)
+            if new_image_crc == last_image_crc:
+                continue
+            timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+            out_png_path = "%s_%s_%d" % (screenshot_base_path,
+                                         timestamp, idx)
+            save_screenshot_b64(out_png_path, img_b64, logger)
             capture_duration = time() - t0
             logger.info("Save_screenshot took %0.1f on %s Visit ID: %d" %
                         (capture_duration, driver.current_url, visit_id))
