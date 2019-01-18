5a36d4bf446f4079722fbccbbbd0eeac484b6a3d
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-01-18 13:10:39 -0500

    Take debug as cmd line argument, refactor state keeping code.

--- segment_pilot_crawl.py
+++ segment_pilot_crawl.py
@@ -1,11 +1,25 @@
+import sys
 from automation import TaskManager, CommandSequence
 from automation.Errors import CommandExecutionError
 import time
 import os
+from os.path import expanduser
 from automation.Commands.utils.screen_capture import capture_screenshots
 from urlparse import urlparse
 
+CURRENT_SITE_INDEX_FILE = expanduser('~/.openwpm/current_site_index')
+REBOOT_FILE = expanduser('~/.openwpm/reboot')
+CRAWL_DONE_FILE = expanduser('~/.openwpm/crawl_done')
+
 DEBUG = False
+if len(sys.argv) > 1 and sys.argv[1] == "--debug":
+    print "DEBUG mode enabled"
+    DEBUG = True
+
+
+def write_to_file(file_path, data):
+    with open(expanduser(file_path), 'w') as f:
+        f.write(str(data))
 
 
 # The list of sites that we wish to crawl
@@ -29,10 +43,12 @@ manager_params['testing'] = DEBUG
 # Read the site list
 sites = []
 
+
 if DEBUG:
-    sites = ['https://www.jcpenney.com/p/xersion-tunic-top/ppr5007814157?pTmplType=regular',
-             #'https://www.aaghalalfoods.jp/product/green-chillies-200-g/'
-             ]
+    sites = [
+        'https://www.jcpenney.com/p/xersion-tunic-top/ppr5007814157?pTmplType=regular',
+        # 'https://www.aaghalalfoods.jp/product/green-chillies-200-g/'
+        ]
 else:
     for l in open("500-product-links.csv"):
         sites.append(l.rstrip())
@@ -50,17 +66,24 @@ for i in xrange(NUM_BROWSERS):
 start_time = time.time()
 
 # Manage control files
-if not os.path.isdir(os.path.expanduser('~/.openwpm/')):
-    os.mkdir(os.path.expanduser('~/.openwpm/'))
-if os.path.isfile(os.path.expanduser('~/.openwpm/reboot')):
-    os.remove(os.path.expanduser('~/.openwpm/reboot'))
-if os.path.isfile(os.path.expanduser('~/.openwpm/current_site_index')):
-    with open(os.path.expanduser('~/.openwpm/current_site_index'), 'r') as f:
-        start_index = int(f.read()) + 1
-    end_index = start_index + NUM_BATCH
-else:
+if DEBUG:
     start_index = 0
     end_index = NUM_BATCH + 1
+else:
+    if not os.path.isdir(expanduser('~/.openwpm/')):
+        os.mkdir(expanduser('~/.openwpm/'))
+    if os.path.isfile(REBOOT_FILE):
+        os.remove(REBOOT_FILE)
+    if os.path.isfile(CURRENT_SITE_INDEX_FILE):
+        with open(CURRENT_SITE_INDEX_FILE, 'r') as f:
+            start_index = int(f.read()) + 1
+        end_index = start_index + NUM_BATCH
+    else:
+        start_index = 0
+        end_index = NUM_BATCH + 1
+
+print "Will start from index %s" % start_index
+
 
 manager = TaskManager.TaskManager(manager_params, browser_params,
                                   process_watchdog=True)
@@ -83,21 +106,20 @@ for i in range(start_index, end_index):
         cs.run_custom_function(capture_screenshots, (TIME_ON_PAGE,),
                                timeout=TIME_ON_PAGE+5)
         manager.execute_command_sequence(cs)
-        with open(
-                os.path.expanduser('~/.openwpm/current_site_index'),
-                'w') as f:
-            f.write(str(i))
+        if not DEBUG:
+            write_to_file(CURRENT_SITE_INDEX_FILE, str(i))
     except CommandExecutionError:
-        with open(os.path.expanduser('~/.openwpm/reboot'), 'w') as f:
-            f.write(str(1))
+        if not DEBUG:
+            write_to_file(REBOOT_FILE, str(1))
         break
 
 print "CLOSING TaskManager after batch"
 manager.close()
 
+
 # Remove index file if we are done
-if current_index >= TOTAL_NUM_SITES:
-    os.remove(os.path.expanduser('~/.openwpm/current_site_index'))
-    with open(os.path.expanduser('~/.openwpm/crawl_done'), 'w') as f:
-        f.write(str(1))
+if not DEBUG and current_index >= TOTAL_NUM_SITES:
+    os.remove(CURRENT_SITE_INDEX_FILE)
+    write_to_file(CRAWL_DONE_FILE, str(1))
+
 print "Total time: " + str(time.time() - start_time)
