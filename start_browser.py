7f7cf3f79e71ce4d5f5c0b0449af886a23200fdf
englehardt <englehardt@gmail.com>
2019-03-04 23:17:12 -0800

    Use better directory for sample script

--- start_browser.py
+++ start_browser.py
@@ -10,8 +10,8 @@ browser_params[0]['cookie_instrument'] = True
 browser_params[0]['js_instrument'] = True
 browser_params[0]['save_javascript'] = True
 
-manager_params['data_directory'] = '~/Desktop/'
-manager_params['log_directory'] = '~/Desktop/'
+manager_params['data_directory'] = '~/Desktop/OpenWPM/'
+manager_params['log_directory'] = '~/Desktop/OpenWPM/'
 
 manager = TaskManager.TaskManager(manager_params, browser_params)
 command_sequence = CommandSequence.CommandSequence("about:newtab")
